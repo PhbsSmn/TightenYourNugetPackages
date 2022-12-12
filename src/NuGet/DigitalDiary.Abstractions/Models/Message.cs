06222e3f238f083e67cb5b6de949c48339e7febe
Pieter Samyn <pieter.samyn@zf.com>
2022-12-12 23:47:41 +0100

    Add project files.

---
+++ src/NuGet/DigitalDiary.Abstractions/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalDiary.Models;
+
+/// <summary>
+/// The message written in the digital diary
+/// </summary>
+/// <param name="Id">The id</param>
+/// <param name="Value">The message</param>
+/// <param name="Timestamp">The time when the message was written</param>
+public record Message(int Id, string Value, DateTime Timestamp);
---
+++ src/WebApi/MyDigitalDiary.WebApi/Program.cs
@@ -0,0 +1,30 @@
+using DigitalDiary;
+using DigitalDiary.MemoryRepository;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IDigitalDiaryRepository, Repository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/diary", (IDigitalDiaryRepository repository, [FromBody] string message) =>
+{
+    repository.Write(message);
+});
+
+
+app.Run();
